a4491f47025cf2c5246e4c136116a68241dc9962
gabriel-shulman-1 <ges.over@gmail.com>
2023-10-02 21:36:58 -0300

    first commit

---
+++ ProductManager.js
@@ -0,0 +1,86 @@
+class ProductManager{
+
+    constructor () {
+        this.products = []
+    }
+
+    getProducts() {
+        return this.products
+    }
+
+    addProduct (title,description,price, Thumbnail, code, stock){
+        if(!title||!description||!price||!Thumbnail||!code||!stock) {
+            console.error("There are fields that are not complete")
+            return
+        }
+
+        const product = this.products.find((e)=>e.code===code)
+
+        if(product){
+            console.log("The code " + code + " exist and the product will not be entered")
+            return
+        }
+        
+        const newProduct = {
+            id: this.products.length + 1,
+            title,
+            description,
+            price,
+            Thumbnail,
+            code,
+            stock,
+        }
+        
+        this.products.push(newProduct)
+        console.log("Product added")
+    }
+
+    getProductById(id){
+        for (let index = 0; index < this.products.length; index++) {
+            if(id===this.products[index].id){
+                console.log("Product with id " + id + " founded." )
+                return this.products[index]
+            }
+        }
+        return(
+            console.log("The product with the id " + id + " was not found.")
+        )
+    }
+    
+}
+
+const productManager = new ProductManager()
+productManager.addProduct(
+    "Producto de prueba",
+    "Esto es un producto de prueba",
+    200,
+    "Sin imagen",
+    "abc123",
+    25
+)
+productManager.addProduct(
+    "Producto de prueba 2",
+    "Esto es un producto de prueba 2",
+    200,
+    "Sin imagen",
+    "abc124",
+    26
+)
+productManager.addProduct(
+    "Producto de prueba 3",
+    "Esto es un producto de prueba 3",
+    200,
+    "Sin imagen",
+    "abc125",
+    26
+)
+productManager.addProduct(
+    "Producto de prueba 3",
+    "Esto es un producto de prueba 3",
+    200,
+    "Sin imagen",
+    "abc125",
+    26
+)
+console.log("get products: ", productManager.getProducts())
+console.log(productManager.getProductById(4))
